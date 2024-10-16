9f4b88225e03709ed12d67189c9abf516ba273c0
LDarkClaw <153105341+LDarkClaw@users.noreply.github.com>
2024-10-16 18:51:01 +0300

    01.py v0.1

--- 16.10.2024/01.py
+++ 16.10.2024/01.py
@@ -1,10 +1,12 @@
 """Написать функцию для генерации строки из n строчных латинских букв"""
 import  random
+from random import randint
+
 
 def generate_string(n):
     lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
-    return  ' '.join(random.choice(lowercase_letters) for _ in range(n))
+    return  ''.join(random.choice(lowercase_letters) for _ in range(n))
 
-string = generate_string(8)
+string = generate_string(randint(1,50))
 print(f"Генерированная строка длины {len(string)}:")
 print(string)
