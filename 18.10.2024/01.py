62d28ac800d40dc94e83f85f9634ae8a5767d0dc
LDarkClaw <153105341+LDarkClaw@users.noreply.github.com>
2024-10-20 11:21:13 +0300

    Update 01.py

--- 18.10.2024/01.py
+++ 18.10.2024/01.py
@@ -2,14 +2,14 @@ import requests
 
 def get_lang():
     languages={0:'ru',1:'en'}
-    choice=int(input("На каком языке вы хотите цитату?: 0 - Русский | 1 - Английский  -- "))
-    return languages.get(choice)
+    choice=(input("На каком языке вы хотите цитату?: 0 - Русский | 1 - Английский  -- "))
 
+    url = 'http://api.forismatic.com/api/1.0/'
+    payload  = {'method': 'getQuote', 'format': 'text', 'lang': languages[int(choice)]}
+    res = requests.get(url, params=payload)
 
-url = 'http://api.forismatic.com/api/1.0/'
-payload  = {'method': 'getQuote', 'format': 'text', 'lang': get_lang()}
-res = requests.get(url, params=payload)
+    data = res.text
 
-data = res.text
+    print(data)
 
-print(data)
+get_lang()
